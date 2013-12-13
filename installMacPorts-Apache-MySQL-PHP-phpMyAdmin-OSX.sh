1fc66be7b2da58a73e9c24785c689de788c32646
enekochan <enekochan@tech.enekochan.com>
2013-12-13 18:57:41 +0100

    Added Mac OS X Mavericks 10.9 and fixed a small bug in the if with version 10.6 and 10.7

--- installMacPorts-Apache-MySQL-PHP-phpMyAdmin-OSX.sh
+++ installMacPorts-Apache-MySQL-PHP-phpMyAdmin-OSX.sh
@@ -83,14 +83,16 @@ LOCALHOST_VIRTUAL_HOST=$result
 VERSION=`sw_vers -productVersion`
 VERSION=${VERSION:3:1}
 if [ "$VERSION" == "6" ]; then
-  URL=https://distfiles.macports.org/MacPorts/MacPorts-2.1.2-10.7-Lion.pkg
+  URL=https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.6-SnowLeopard.pkg
 elif [ "$VERSION" == "7" ]; then
-  URL=https://distfiles.macports.org/MacPorts/MacPorts-2.1.2-10.6-SnowLeopard.pkg
+  URL=https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.7-Lion.pkg
 elif [ "$VERSION" == "8" ]; then
-  URL=https://distfiles.macports.org/MacPorts/MacPorts-2.1.2-10.8-MountainLion.pkg
+  URL=https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.8-MountainLion.pkg
+elif [ "$VERSION" == "9" ]; then
+  URL=https://distfiles.macports.org/MacPorts/MacPorts-2.2.1-10.9-Mavericks.pkg
 fi
 if [ "$URL" == "" ]; then
-  echo "MacPort can only be installed automatically in Mac OS X 10.6, 10.7 o 10.8"
+  echo "MacPort can only be installed automatically in Mac OS X 10.6, 10.7, 10.8 or 10.9"
   exit
 fi
 curl -O $URL
